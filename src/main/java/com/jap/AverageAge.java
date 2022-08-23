d79d3ea2fc717e82743640f8a9bf4ddecf404872
Korni Arjun Reddy <arjunreddykorni@gmail.com>
2022-08-23 21:23:00 +0530

    implemented as per the question.

--- src/main/java/com/jap/AverageAge.java
+++ src/main/java/com/jap/AverageAge.java
@@ -5,14 +5,20 @@ public class AverageAge {
     public String averageAgeCalculator(String[] ageRawData)
     {
         double avg = 0;
+        double total =0;
 
         try{
             //Write the logic
+            for (int i = 0; i < ageRawData.length ; i++) {
+                total =  total + Integer.parseInt(ageRawData[i]) ;
+                avg = total/ageRawData.length;}
 
             //Handle specific exception
-        }catch (Exception exception)
+        }
+            catch (NumberFormatException e)
         {
-            return exception.toString();
+            System.out.println("e = " + e);
+            return e.toString();
         }
         return String.valueOf(avg);
     }
